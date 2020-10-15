956b88f72ffba2fd7808b20afa9c1a8aab57b188
Chris Maxwell <chmaxwel@icloud.com>
2020-10-15 16:14:38 +0200

    ++offline object store

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import CartItem from'../CartItem';
 import Auth from '../../utils/auth';
 import './style.css';
 import { useStoreContext } from '../../utils/GlobalState';
-import { TOGGLE_CART } from '../../utils/actions';
+import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from '../../utils/actions';
+import {idbPromise} from '../../utils/helpers';
 
 const Cart = () => {
     const [state, dispatch] = useStoreContext();
+
+    // check if anything in state cart property on load
+    useEffect(() => {
+        async function getCart() {
+            const cart = await idbPromise('cart', 'get');
+            dispatch({
+                type: ADD_MULTIPLE_TO_CART,
+                products: [...cart]
+            });
+        };
+
+        if (!state.cart.length) {
+            getCart();
+        }
+    }, [state.cart.length, dispatch]);
 
     function toggleCart() {
         dispatch({ type: TOGGLE_CART });
