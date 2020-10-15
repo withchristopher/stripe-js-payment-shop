fa6dfc3a820c32c5d7b22b07f0520bdfe31bb1be
Chris Maxwell <chmaxwel@icloud.com>
2020-10-15 21:46:49 +0200

    ++ useMutation & stripe success

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import ApolloClient from 'apollo-boost';
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
+import Success from "./pages/Success";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Nav from "./components/Nav";
@@ -37,6 +38,7 @@ function App() {
             <Route exact path="/signup" component={Signup} />
             <Route exact path="/orderHistory" component={OrderHistory} />
             <Route exact path="/products/:id" component={Detail} />
+            <Route exact path="/success" component={Success} />
             <Route component={NoMatch} />
           </Switch>
           </StoreProvider>
